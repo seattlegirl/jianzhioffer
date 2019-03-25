62dfaff804590c5d01f7a0e074348c9d19688834
seattlegirl <likaiyue@i.shu.edu.cn>
2019-03-25 16:31:35 +0800

    剑指offer：从尾到头打印链表
    Signed-off-by: seattlegirl <likaiyue@i.shu.edu.cn>

---
+++ reverse-linked-list.js
@@ -0,0 +1,36 @@
+function ListNode(x){
+    this.val = x;
+    this.next = null;
+}
+//法一
+var reverseList = function(head) {
+    if (head==null || head.next==null){
+       return head;
+   }
+   var pre = null;
+   var next = null;
+   while (head != null) {
+       next = head.next;
+       head.next = pre;
+       pre = head;
+       head = next;
+   }
+   return pre;  
+};
+
+//法二
+var reverseList = function(head) {
+   if(head==null || head.next==null){
+       return head;
+   }
+   var list = head;
+   var p = list;
+   var q= null;
+   while(p.next!=null) {
+       q = p.next;
+       p.next = q.next;
+       q.next = list;
+       list = q;     
+   }
+   return list;
+}
---
+++ reverse-linked-list.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # 返回从尾部到头部的列表值序列，例如[1,2,3]
+    def printListFromTailToHead(self, listNode):
+        # write code here
+        p=[]
+        while listNode:
+            p.append(listNode.val)
+            listNode=listNode.next
+        return p[::-1]
+if __name__ == "__main__":
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+    l=[]
+    l=Solution().printListFromTailToHead(head)
+    print(l)
+    
+            
